37225e098775513a16fab56a821b7252e071cc57
Julio Dutra Nicacio <julio.nicacio@mnstrstudio.com>
2024-06-16 18:33:51 -0300

    Fix Camera's Pitch on multiplayer

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -255,6 +255,13 @@ void ABlasterCharacter::AimOffset(float DeltaTime)
 	}
 
 	AO_Pitch = GetBaseAimRotation().Pitch;
+	if(AO_Pitch > 90.0f && !IsLocallyControlled())
+	{
+		// Map Pitch from [270, 360] to [-90, 0]
+		FVector2D InRange(270.f, 360.f);
+		FVector2D OutRange(-90.f, 0.f);
+		AO_Pitch = FMath::GetMappedRangeValueClamped(InRange, OutRange, AO_Pitch);
+	}
 }
 
 
