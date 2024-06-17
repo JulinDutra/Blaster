2525f8717df15187b697fb020e5ea770eda0e11b
Julio Dutra Nicacio <julio.nicacio@mnstrstudio.com>
2024-06-16 21:32:03 -0300

    Update NetUpdateFrequency

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -43,6 +43,9 @@ ABlasterCharacter::ABlasterCharacter()
 	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
 
 	TurningInPlace = ETurningInPlace::ETIP_NotTurning;
+
+	NetUpdateFrequency = 66.f;
+	MinNetUpdateFrequency = 33.f;
 }
 
 void ABlasterCharacter::Tick(float DeltaSeconds)
