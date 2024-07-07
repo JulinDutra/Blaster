5e9e625d9d23251a6356bce9c3e65f1a89b5a7e5
Julio Dutra Nicacio <julio.nicacio@mnstrstudio.com>
2024-07-07 20:02:17 -0300

    Add Projectile Weapon class

---
+++ Source/Blaster/Weapon/BlasterProjectileWeapon.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BlasterWeapon.h"
+#include "BlasterProjectileWeapon.generated.h"
+
+UCLASS()
+class BLASTER_API ABlasterProjectileWeapon : public ABlasterWeapon
+{
+	GENERATED_BODY()
+};
